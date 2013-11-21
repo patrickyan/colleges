beecaa8de917abcd363d402e6a843abb8f484a1d
Pat <pat.yan@gmail.com>
2013-11-21 09:59:58 -0600

    Initial commit

---
+++ scraper.js
@@ -0,0 +1,21 @@
+$(document).ready( function() {
+	var collegeLinks = $('.institution');
+	var colleges = {};
+	var collegesStringified = '';
+	var result;
+
+	for (var i = 0; i < collegeLinks.length; i++) {
+		var $collegeLinkItem = $(collegeLinks[i])
+		var collegeUrl = $collegeLinkItem.attr('href').replace(/http:\/\//, '').replace(/www./, '').replace(/\//, '');
+		var collegeName = $collegeLinkItem.text();
+		colleges[collegeUrl] = {
+			name: collegeName
+		};
+	}
+
+	collegesStringified = JSON.stringify(colleges, null, 2);
+
+	$result = $('<div id="scraped-result"/>');
+	$result.append( $('<textarea/>').text( collegesStringified ) );
+	$('body').append($result);
+});
